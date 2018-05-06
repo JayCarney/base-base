f3e5bbe4f9ae369ddbb6342712836924ebf0b5aa
Jason Carney <jascarne@justin.tv>
2018-05-05 20:48:49 -0500

    programmatically determine the adjustments and manually adjust at sizes that require it instead of manually adding all font adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
 
 gulp.task('connect', function(){
   connect.server({
@@ -10,7 +13,7 @@ gulp.task('connect', function(){
 });
 
 // keeps gulp from crashing for scss errors
-gulp.task('sass', function () {
+gulp.task('sass', ['sass-includes'], function () {
   return gulp.src('./examples/*.scss')
       .pipe(sass({ errLogToConsole: true }))
       .pipe(gulp.dest('./examples/css'))
@@ -23,8 +26,42 @@ gulp.task('livereload', function (){
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./**/*.scss', ['sass']);
+  gulp.watch(['./**/*.scss', '!./**/_all.scss'], ['sass']);
   gulp.watch('./example/**/*', ['livereload']);
+});
+
+
+// Hack the ability to import directories in Sass
+// Find any _all.scss files and write @import statements
+// for all other *.scss files in the same directory
+//
+// Import the whole directory with @import "somedir/all";
+gulp.task('sass-includes', function (callback) {
+	var all = '_all.scss';
+	glob('./**/' + all, function (error, files) {
+		files.forEach(function (allFile) {
+			// Add a banner to warn users
+			fs.writeFileSync(allFile, '/** This is a dynamically generated file **/\n\n');
+
+			var directory = path.dirname(allFile);
+			var partials = fs.readdirSync(directory).filter(function (file) {
+				return (
+					// Exclude the dynamically generated file
+					file !== all &&
+					// Only include _*.scss files
+					path.basename(file).substring(0, 1) === '_' &&
+					path.extname(file) === '.scss'
+				);
+			});
+
+			// Append import statements for each partial
+			partials.forEach(function (partial) {
+				fs.appendFileSync(allFile, '@import "' + partial + '";\n');
+			});
+		});
+	});
+
+	callback();
 });
 
 gulp.task('default', ['connect', 'watch', 'sass']);
